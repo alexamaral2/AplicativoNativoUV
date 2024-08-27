ac2b062fd787c56372d2fe0a6fb58e1d95c8ada1
Alex Junior <alexjunior2018@gmail.com>
2024-08-26 21:37:12 -0300

    Adicionando Desafio 1

---
+++ Desafio1/app/src/main/java/br/com/alexjr/desafio1/MainActivity.java
@@ -0,0 +1,29 @@
+package br.com.alexjr.desafio1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        Button button = findViewById(R.id.btn_secondscreen);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
